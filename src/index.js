96c83abfdbbe4423bfba7903eb3f7588e1b31dcb
Ekterina Goryainova <kateas27@mail.ru>
2024-03-13 14:43:37 +0200

    Реализовать основной компонент блога с роутингом

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Blog from "./blog";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Blog />
+  </BrowserRouter>
 );
